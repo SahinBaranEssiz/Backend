8a8db726d9089f69f801f848253b4c090a280b3b
Şahin Baran Eşsiz <53182048+SahinBaranEssiz@users.noreply.github.com>
2020-09-07 17:00:52 +0300

    Create LastFlightController.php

---
+++ src/Controller/Api/LastFlightController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Builder\TransactionBuilder;
+use App\Controller\AbstractController;
+use App\Entity\Transaction;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+* @Route("/xx/{id}", name="xx")
+*/
+class LastFlightController extends AbstractController
+{
+
+    private function getLastTransaction($user)
+    {
+        return $this->getDoctrine()
+            ->getRepository(Transaction::class)
+            ->findOneBy(
+                ['user' => $user],
+                ['ticketDate'=>'DESC']
+            );
+    }
+
+
+    public function __invoke(User $user): JsonResponse
+    {
+        dd($user->getTransactions()->first());
+        if($user){
+            if($transaction = $this->getLastTransaction($user)){
+                $data = new TransactionBuilder($transaction); //reset for getting array first element
+                $response = $this->successJsonResponse(array($data));
+            }else{
+                $data = ["Transaction bulunamadı..."];
+                $response = $this->errorJsonResponse(array($data));
+            }
+        }else{
+            $data = ["Kullanıcı bulunamadı..."];
+            $response = $this->errorJsonResponse(array($data));
+        }
+        return $response;
+    }
+
+}
